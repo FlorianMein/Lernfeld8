e7d0cf9d04ef7c6ffe4c869b55038781bd36cf56
selim-asik <73435455+selim-asik@users.noreply.github.com>
2023-10-04 13:27:12 +0200

    Demo Tests Project created

---
+++ Itech-Attendance/Itech-Attendance.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Itech_Attendance.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
