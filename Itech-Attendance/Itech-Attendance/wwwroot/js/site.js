e8b605654628f85a98284df98fd773b993ef8b47
selim-asik <73435455+selim-asik@users.noreply.github.com>
2023-10-06 10:52:33 +0200

    Countdown etc

--- Itech-Attendance/Itech-Attendance/wwwroot/js/site.js
+++ Itech-Attendance/Itech-Attendance/wwwroot/js/site.js
@@ -3,4 +3,43 @@
 
 // Write your JavaScript code.
 
+var countDownDate = new Date(new Date().getTime() + 1 * 60000)
 
+// Update the count down every 1 second
+var x = setInterval(function () {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+
+
+        var classNameValue = document.getElementById("classNameInput").value;
+        console.log(classNameValue);
+
+        fetch("/Home/GenerateNewQR",
+            {
+                method: "POST",
+                body: JSON.stringify({
+                    className: classNameValue
+                })
+            })
+            .then(function (res) { console.log(classNameValue) })
+    }
+}, 1000);
